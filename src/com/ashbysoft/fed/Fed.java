a342f40baa6edb02a341320f6715a5e6d665b446
Phil Ashby <phil.github@ashbysoft.com>
2023-04-09 14:59:31 +0100

    dogfooding time! start of a self-hosted font editor tool

---
+++ src/com/ashbysoft/fed/Fed.java
@@ -0,0 +1,9 @@
+package com.ashbysoft.fed;
+
+// Font EDitor..
+
+public class Fed {
+    public static void main(String[] args) {
+        System.out.println("--- (Ashbysoft *) Swingland font editor ---");
+    }    
+}
